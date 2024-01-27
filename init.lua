ce904393d0e7ac2aff8b7fe96394ddb0ac94b76a
Connor Keane <connor.keane@kxnr.me>
2024-01-27 14:13:09 -0700

    cmp with autoimport and treesitter plugin

--- init.lua
+++ init.lua
@@ -49,6 +49,7 @@ lsp_cfg.pylsp.setup({
         jedi_completion = {
           enabled = true,
           fuzzy = true,
+          include_params = true,
         },
         ruff = {
           enabled = true,
@@ -90,6 +91,12 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 local cmp = require('cmp')
 cmp.setup({
   preselect = cmp.PreselectMode.None,
@@ -107,15 +114,24 @@ cmp.setup({
     { name = 'emoji' },
   },
   mapping = {
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = false,
-    },
+    ['<CR>'] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    }),
     ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if vim.fn["vsnip#jumpable"](1) == 1 then
         feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
